bfa930ab5e9d76950b8efcd9a204d790c6512314
Graham Fairweather <xotic750@gmail.com>
2019-07-26 00:23:12 +0200

    :bookmark: v2.0.14
    Auto build

--- dist/get-own-enumerable-keys-x.js
+++ dist/get-own-enumerable-keys-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-24T16:45:50.971Z",
+  "date": "2019-07-25T22:23:05.231Z",
   "describe": "",
   "description": "Like Reflect.ownKeys but gets only enumerable properties.",
   "file": "get-own-enumerable-keys-x.js",
   "hash": "895a467d2db4b479621b",
   "license": "MIT",
-  "version": "2.0.13"
+  "version": "2.0.14"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
