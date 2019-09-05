678a6d43fa4869963e79cc969a96cd2e54783d43
Graham Fairweather <xotic750@gmail.com>
2019-09-05 21:52:17 +0200

    Maintenance

--- dist/get-own-enumerable-keys-x.js
+++ dist/get-own-enumerable-keys-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-09-04T23:42:39.718Z",
+  "date": "2019-09-05T19:52:11.221Z",
   "describe": "",
   "description": "Like Reflect.ownKeys but gets only enumerable properties.",
   "file": "get-own-enumerable-keys-x.js",
